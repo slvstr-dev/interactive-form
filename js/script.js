3432617b9c691a321c7600ea6269298600e00321
Sylvester Hofstra <hello@slvstr.dev>
2021-01-23 13:10:16 +0100

    Fix incorrect colorOptions logic

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ const nameInput = document.getElementById("name");
 nameInput.focus();
 
 // Step 4
-const jobsInput = document.getElementById("title");
 const otherInput = document.getElementById("other-job-role");
+const jobsInput = document.getElementById("title");
 
 otherInput.style.display = "none";
 
@@ -21,13 +21,12 @@ const designInput = document.getElementById("design");
 colorInput.disabled = true;
 
 designInput.addEventListener("change", (event) => {
-    console.dir(event.target);
     const selectedDesign = event.target.value;
     const colorOptions = colorInput.querySelectorAll("[data-theme]");
 
     for (let i = 0; i < colorOptions.length; i++) {
         colorOptions[i].hidden =
-            colorOptions[i].dataset.theme === selectedDesign;
+            colorOptions[i].dataset.theme !== selectedDesign;
     }
 
     colorInput.disabled = false;

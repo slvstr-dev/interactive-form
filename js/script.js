c9eea75616ae2d2a11e018585cee880eaae3783a
Sylvester Hofstra <hello@slvstr.dev>
2021-01-23 12:36:31 +0100

    Fix broken otherInput field display toggle

--- js/script.js
+++ js/script.js
@@ -1,34 +1,34 @@
- // Step 3
- const nameInput = document.getElementById("name");
+// Step 3
+const nameInput = document.getElementById("name");
 
- nameInput.focus();
+nameInput.focus();
 
- // Step 4
- const jobsInput = document.getElementById("title");
- const otherInput = document.getElementById("other-job-role");
+// Step 4
+const jobsInput = document.getElementById("title");
+const otherInput = document.getElementById("other-job-role");
 
- jobsInput.addEventListener("change", event => {
-     other.style.display = event.target.value === "other" ?
-         "inline-block" :
-         "none";
- });
+otherInput.style.display = "none";
 
- otherInput.style.display = "none";
+jobsInput.addEventListener("change", (event) => {
+    otherInput.style.display =
+        event.target.value === "other" ? "inline-block" : "none";
+});
 
- // Step 5
- const colorInput = document.getElementById("color");
- const designInput = document.getElementById("design");
+// Step 5
+const colorInput = document.getElementById("color");
+const designInput = document.getElementById("design");
 
- colorInput.disabled = true;
+colorInput.disabled = true;
 
- designInput.addEventListener("change", event => {
-     console.dir(event.target);
-     const selectedDesign = event.target.value;
-     const colorOptions = colorInput.querySelectorAll("[data-theme]");
+designInput.addEventListener("change", (event) => {
+    console.dir(event.target);
+    const selectedDesign = event.target.value;
+    const colorOptions = colorInput.querySelectorAll("[data-theme]");
 
-     for (let i = 0; i < colorOptions.length; i++) {
-         colorOptions[i].hidden = colorOptions[i].dataset.theme === selectedDesign;
-     }
+    for (let i = 0; i < colorOptions.length; i++) {
+        colorOptions[i].hidden =
+            colorOptions[i].dataset.theme === selectedDesign;
+    }
 
-     colorInput.disabled = false;
- });
+    colorInput.disabled = false;
+});
